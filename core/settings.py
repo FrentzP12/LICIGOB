96c2adeed82af06124796ff5ca1f4933b8a8f1f2
Frentz Diego Padilla Gonzales <frentz233@gmail.com>
2025-05-30 21:04:22 -0500

    upd

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'),'.ngrok.io','.ngrok-free.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'),'.ngrok.io','.ngrok-free.app','web-production-fb3b6.up.railway.app']
 
 # Application definition
 
